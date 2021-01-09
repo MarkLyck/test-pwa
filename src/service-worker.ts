bee09246045ee319133a0c57009747a3e479b2ac
Mark Lyck <mark.lyck@gmail.com>
2021-01-09 11:20:31 -0500

    :bulb: Add warning comment to service-worker

--- src/service-worker.ts
+++ src/service-worker.ts
@@ -1,6 +1,12 @@
 /// <reference lib="webworker" />
 /* eslint-disable no-restricted-globals */
 
+// IMPORTANT!
+// ---------
+// This file must stay in the ./src folder.
+// If it is moved, service-worker will break.
+// ---------
+
 // This service worker can be customized!
 // See https://developers.google.com/web/tools/workbox/modules
 // for the list of available Workbox modules, or add any other
