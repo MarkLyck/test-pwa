fb5f7adc533aba1d52ccccb1e9cdc0f30c832849
Mark Lyck <mark.lyck@gmail.com>
2021-01-22 18:57:53 -0500

    :wrench: fix lighthouse assertions

--- .lighthouserc.js
+++ .lighthouserc.js
@@ -5,19 +5,20 @@ module.exports = {
         },
         assert: {
             preset: 'lighthouse:all',
-            bypass: 'off',
-            "color-contrast": "off",
-            "maskable-icon": "off",
-            "unsized-images": "off",
-            "unused-css-rules": ["error", { "maxLength": 2 }],
-            "unused-javascript": ["error", { "maxLength": 1 }],
-            "first-contentful-paint": ["error", { "minScore": 0.5 }],
-            "first-meaningful-paint": ["error", { "minScore": 0.5 }],
-            "largest-contentful-paint": ["error", { "minScore": 0.5 }],
-            "legacy-javascript": ["error", { "maxLength": 1 }],
-            "max-potential-fid": ["error", { "minScore": 0.1 }],
-            "render-blocking-resources": ["error", { "maxLength": 2 }],
+            "assertions": {
+                "bypass": 'off',
+                "color-contrast": "off",
+                "maskable-icon": "off",
+                "unsized-images": "off",
+                "unused-css-rules": ["error", { "maxLength": 2 }],
+                "unused-javascript": ["error", { "maxLength": 1 }],
+                "first-contentful-paint": ["error", { "minScore": 0.5 }],
+                "first-meaningful-paint": ["error", { "minScore": 0.5 }],
+                "largest-contentful-paint": ["error", { "minScore": 0.5 }],
+                "legacy-javascript": ["error", { "maxLength": 1 }],
+                "max-potential-fid": ["error", { "minScore": 0.1 }],
+                "render-blocking-resources": ["error", { "maxLength": 2 }]
+            }
         },
-
     },
 };
